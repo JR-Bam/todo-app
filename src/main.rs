0ee7a3074022cf7fc217c3e56574ad1e45d5ba8a
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2024-07-22 22:42:00 +0800

    Added Header and Side bar

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,104 @@
+use eframe::egui::{self, CentralPanel, FontFamily, FontId, Layout, SidePanel, TextStyle, TopBottomPanel, ViewportBuilder};
+use todo_func::{json_parser, TodoApp};
+
 mod todo_func;
 
-fn main() {
-    let mut storage = todo_func::read_state_from_file().unwrap_or_default();
-    storage.list.insert("Bambibfsnjfsdfndsmnam".to_string(), false);
+const PADDING: f32 = 5.0;
 
-    todo_func::save_state_to_file(&storage).expect("Failed to save state");
+impl TodoApp {
+
+    // * All UI declarations here
+    fn render(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame){
+        if self.show_sidepanel {
+            SidePanel::left("pages_list")
+                .resizable(false)
+                .show(ctx, 
+            |ui|{
+                ui.add_space(PADDING);
+                ui.vertical_centered_justified(|ui|{
+                    ui.heading("List of Pages");
+                });
+                ui.separator();
+                ui.monospace("WIP: You can put different pages here that each houses a set of notes");
+            });
+        }
+
+        CentralPanel::default().show(ctx, |_|{
+            self.render_header(ctx);
+        });
+    }
+    fn render_header(&mut self, ctx: &eframe::egui::Context) {
+        TopBottomPanel::top("header").show(ctx, |ui| {
+
+            egui::menu::bar(ui, |ui| {
+                // * Sidebar button container
+                ui.with_layout(Layout::left_to_right(egui::Align::Center), |ui|{
+
+                    if ui.button("☰")
+                    .on_hover_text_at_pointer("Menu")
+                    .clicked() {
+                        self.show_sidepanel = !self.show_sidepanel;
+                    }
+
+                    ui.add_space(PADDING);
+
+                    if ui.button("⚙")
+                    .on_hover_text_at_pointer("Settings")
+                    .clicked() {
+                        // TODO: Settings Functionality
+                    }
+                    
+                });
+
+                ui.with_layout(Layout::right_to_left(egui::Align::Center), |ui|{
+                    if ui.button("➕ Add Note")
+                    .clicked() {
+                        // TODO: Add Note Functionality 
+                    }
+                });
+                
+            });
+
+        });
+    }
+
+    // * Setup stuff here like fonts, etc.
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        configure_fonts(&cc.egui_ctx);
+        Self {
+            state: json_parser::read_state_from_file().unwrap_or_default(),
+            show_sidepanel: false
+        }
+    }
+}
+
+fn configure_fonts(ctx: &egui::Context){
+    use FontFamily::{Monospace, Proportional};
+
+    let mut style = (*ctx.style()).clone();
+    style.text_styles = [
+        (TextStyle::Heading, FontId::new(20.0, Proportional)),
+        (TextStyle::Body, FontId::new(16.0, Proportional)),
+        (TextStyle::Monospace, FontId::new(12.0, Monospace)),
+        (TextStyle::Button, FontId::new(16.0, Proportional)),
+        (TextStyle::Small, FontId::new(8.0, Proportional)),
+    ]
+    .into();
+
+    ctx.set_style(style);
+}
+
+fn main() -> eframe::Result {
+    let default_options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default().with_inner_size([800.0, 500.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "TODO App", 
+        default_options,
+        Box::new(|cc|{
+            Ok(Box::new(TodoApp::new(cc)))
+        })
+    )
 }
--- src/todo_func.rs
+++ src/todo_func.rs
@@ -1,27 +1,54 @@
-use std::{collections::HashMap, fs::File, io::{self, Read, Write}, path::Path};
+use std::collections::HashMap;
 
+use eframe::App;
 use serde::{Deserialize, Serialize};
 
-const FILE_PATH: &str = "entries.json";
 
 #[derive(Serialize, Deserialize, Default)]
 pub struct AppState {
     pub list: HashMap<String, bool>
 }
 
-pub fn read_state_from_file() -> io::Result<AppState> {
-    if Path::new(FILE_PATH).exists() {
-        let mut file = File::open(FILE_PATH)?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-        serde_json::from_str(&contents).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
-    } else {
-        Ok(AppState::default())
+pub mod json_parser {
+    use std::{fs::File, io::{self, Read, Write}, path::Path};
+    use super::AppState;
+
+    const FILE_PATH: &str = "entries.json";
+
+    pub fn read_state_from_file() -> io::Result<AppState> {
+        if Path::new(FILE_PATH).exists() {
+            let mut file = File::open(FILE_PATH)?;
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)?;
+            serde_json::from_str(&contents).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+        } else {
+            Ok(AppState::default())
+        }
+    }
+
+    pub fn save_state_to_file(state: &AppState) -> io::Result<()> {
+        let json = serde_json::to_string_pretty(state)?;
+        let mut file = File::create(FILE_PATH)?;
+        file.write_all(json.as_bytes())
     }
 }
 
-pub fn save_state_to_file(state: &AppState) -> io::Result<()> {
-    let json = serde_json::to_string_pretty(state)?;
-    let mut file = File::create(FILE_PATH)?;
-    file.write_all(json.as_bytes())
+#[derive(Default)]
+pub struct TodoApp{
+    pub state: AppState,
+    pub show_sidepanel: bool
+}
+
+impl App for TodoApp {
+
+    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {
+        json_parser::save_state_to_file(&self.state)
+            .expect("Failed to save state.");
+    }
+
+    fn raw_input_hook(&mut self, _ctx: &eframe::egui::Context, _raw_input: &mut eframe::egui::RawInput) {}
+    
+    fn update(&mut self, ctx: &eframe::egui::Context, frame: &mut eframe::Frame) {
+        self.render(ctx, frame);
+    }
 }
